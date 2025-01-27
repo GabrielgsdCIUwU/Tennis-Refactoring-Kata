51d2dcb971346aa1814710f912388ef03970df3d
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-01-27 09:36:39 +0100

    feat(test): Añadir casos test parametrizados

---
+++ test/tennis_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.tennis1 import TennisGame1
+
+from test.tennis_unittest import play_game, test_cases
+
+
+@pytest.mark.parametrize(
+    "TennisGameClass",
+    [
+        TennisGame1
+    ],
+)
+@pytest.mark.parametrize(
+    "p1_points p2_points score p1_name p2_name".split(), test_cases
+)
+
+def test_get_score_most_games(
+    TennisGameClass, p1_points, p2_points, score, p1_name, p2_name
+):
+    game = play_game(TennisGameClass, p1_points, p2_points, p1_name, p2_name)
+    assert score == game.score()
+
